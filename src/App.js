eb5658351a0a430c82bb6b7cdfb65bad7bc9c658
Daniel Moreira <danieldenis01@gmail.com>
2022-04-27 21:16:29 -0300

    Adiciona postMessage para receber evento de início

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
+import React from 'react';
 import './App.css';
 import 'scorm-again';
 
-const settings = {}
+class App extends React.Component {
+  constructor(props) {
+    super(props);
 
-function App() {
-  const scormRootFile = document.body.getAttribute('data-scorm-root')
-  // eslint-disable-next-line no-undef
-  window.API = new Scorm12API(settings)
+    this.state = {
+      scormRootFile: document.body.getAttribute('data-scorm-root'),
+      isReady: false
+    }
 
-  window.API.on("LMSInitialize", () => {
-    const params = { type: "LMSInitialize" }
-    window.top.postMessage(params, '*')
-  })
+    window.addEventListener('message', ({ data }) => {
+      if (data.type === 'ScormInitialize') {
+        this.initScorm(data.payload)
+      }
+    })
+  }
 
-  window.API.on("LMSSetValue.cmi.*", (CMIElement, value) => {
-    const params = { type: "LMSSetValue.cmi.*", payload: [CMIElement, value] }
-    window.top.postMessage(params, '*')
-  })
+  render() {
+    const { isReady, scormRootFile } = this.state
 
-  return (
-    <iframe className="eic-scorm-frame" src={ scormRootFile } title="Escola IC"></iframe>
-  );
+    return (
+      <div className="app">
+        {(function() {
+          if (isReady) {
+            return <iframe className = "eic-scorm-frame" src = { scormRootFile } title="Scorm Content"></iframe>
+          } else {
+            return <h1>Teste</h1>
+          }
+        })()}
+      </div>
+    );
+  }
+
+  initScorm(settings) {
+    if (this.state.isReady) return
+
+    // eslint-disable-next-line no-undef
+    window.API = new Scorm12API(settings)
+
+    this.setState({ isReady: true })
+
+    window.API.on("LMSInitialize", () => {
+      const params = { type: "LMSInitialize" }
+      window.top.postMessage(params, '*')
+    })
+
+    window.API.on("LMSSetValue.cmi.*", (CMIElement, value) => {
+      const params = { type: "LMSSetValue.cmi.*", payload: [CMIElement, value] }
+      window.top.postMessage(params, '*')
+    })
+  }
 }
 
 export default App;
